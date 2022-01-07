b8f22243865089a79ffe0ecad3d332cf80e55b99
大樹堤 <kani.mono56@gmail.com>
2022-01-07 16:37:54 +0900

    create mock

--- elm-http/src/Main.elm
+++ elm-http/src/Main.elm
@@ -1,8 +1,82 @@
 module Main exposing (main)
 
-import Html exposing (Html, div, text)
+import Html exposing (..)
+import Html.Attributes exposing (alt, class, placeholder, src, type_)
+
+
+view : Html msg
+view =
+    div []
+        [ header
+        , containerLayout
+        ]
+
+
+containerLayout : Html msg
+containerLayout =
+    div [ class "container-lg pt-3" ]
+        [ div [ class "d-flex flex-wrap flex-content-start" ]
+            [ dogCard
+            , dogCard
+            , dogCard
+            , dogCard
+            , dogCard
+            , dogCard
+            , dogCard
+            , dogCard
+            , dogCard
+            ]
+        ]
+
+
+dogCard : Html msg
+dogCard =
+    div [ class "Box color-shadow-small flex-equal m-2" ]
+        [ div [ class "Box-row" ]
+            [ img
+                [ class "img-responsive dog-image"
+                , src "./../public/pexels-christian-domingues-731022.jpg"
+                , alt "dog"
+                ]
+                []
+            ]
+        , div [ class "Box-row" ]
+            [ p [ class "mb-0 color-fg-muted" ]
+                [ text """
+                text text text
+                text text text
+                text text text
+                text text text
+                text text text
+                text text text
+              """
+                ]
+            ]
+        , div [ class "Box-row" ]
+            [ button
+                [ type_ "button"
+                , class "btn btn-primary btn-block"
+                ]
+                [ text "detail dog" ]
+            ]
+        ]
+
+
+header : Html msg
+header =
+    div [ class "Header" ]
+        [ div [ class "Header-item" ] [ text "Search Dog" ]
+        , div [ class "Header-item" ]
+            [ input
+                [ type_ "text"
+                , placeholder "SearchDog"
+                , class "form-control Header-input"
+                ]
+                []
+            ]
+        ]
 
 
 main : Html msg
 main =
-    div [] [ text "hello, Dog" ]
+    view
